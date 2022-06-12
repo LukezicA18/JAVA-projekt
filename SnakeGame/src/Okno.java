954944b7f4fb9505c41d1b4673d34fb85f60a4c8
LukezicA18 <49947160+LukezicA18@users.noreply.github.com>
2022-06-13 00:01:02 +0200

    Update Okno.java

--- SnakeGame/src/Okno.java
+++ SnakeGame/src/Okno.java
@@ -3,7 +3,6 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -16,9 +15,8 @@ public class Okno extends JFrame {
 		super();
 		this.snake = snake;
 		setTitle("Snake");
-		setMinimumSize(new Dimension((int)(snake.OknoSirina * 1.1), (int)(snake.OknoVisina * 1.17)));
+		setMinimumSize(new Dimension((int)(snake.OKNO_SIRINA * 1.1), (int)(snake.OKNO_VISINA * 1.17)));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
 		add(new Panel(snake), BorderLayout.CENTER);
 		
 		// Meni 
@@ -26,30 +24,15 @@ public class Okno extends JFrame {
 		setJMenuBar(meniVrstica);
 		JMenu meni = new JMenu("Igra");
 		JMenuItem novaIgra = new JMenuItem("Nova igra");
-		JMenuItem moznosti = new JMenuItem("Mo�nosti");
-//		JMenuItem novaIgra = new JMenuItem("Nova igra");
-//		JMenuItem novaIgra = new JMenuItem("Nova igra");
-//		JMenuItem novaIgra = new JMenuItem("Nova igra");
-		novaIgra.addActionListener(new ActionListener() {
-			
+		novaIgra.addActionListener(new ActionListener() { 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				snake.ponovno();
+				if (snake.isKoncano())
+					snake.ponovno();
 				}});
 		
-		moznosti.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				drugoOkno = new JFrame();
-				
-				}});
-
 		meni.add(novaIgra);
-		meni.add(moznosti);
 		meniVrstica.add(meni);
-		//
-		
 		pack();
 	}
 
